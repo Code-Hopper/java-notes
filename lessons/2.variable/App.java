496ece01b4811748aa058afa4591287aa5af8da7
code-hopper <ameykhondekar01@gmail.com>
2025-05-23 14:35:25 +0530

    java notes

---
+++ lessons/2.variable/App.java
@@ -0,0 +1,37 @@
+public class App {
+    public static void main(String[] args) {
+        int data1 = 1000;
+        int data2 = 2000;
+        int data_3 = 3000;
+
+        short data4 = 12;
+        long data5 = 1231231316;
+
+        float number = 1.5f;
+        double number2 = 100.123145;
+        
+        char option = 'a';
+
+        String name = "Amey Khondekar";
+
+        boolean result = false;
+
+        System.out.println(data1);
+        System.out.println(data2);
+        System.out.println(data_3);
+
+        System.out.println(data4);
+        System.out.println(data5);
+
+        System.out.println(number);
+        System.out.println(number2);
+
+        System.out.println(option);
+
+        System.out.println(name);
+        System.out.println(result);
+
+        System.out.println(data1 + data2 + data_3);
+
+    }
+}
