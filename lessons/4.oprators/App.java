239246af5636f8c191b1b64b00db172cd5f4a517
code-hopper <ameykhondekar01@gmail.com>
2025-05-30 12:09:26 +0530

    pre and post

--- lessons/4.oprators/App.java
+++ lessons/4.oprators/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        
+
         int number1 = 123;
         int number2 = 456;
 
@@ -18,6 +18,21 @@ public class App {
         System.out.println(num);
         num -= 10;
         System.out.println(num);
+
+        // pre and post increemen and dcreement
+
+        int data = 10;
+
+        // data++;// post //data = data + 1 11
+        // ++data;// pre //data = data + 1 11
+
+        // System.out.println(data++); // 11
+        // System.out.println(data++); // 11
+        // System.out.println(data); // 11
+
+        System.out.println(++data); // 11
+        System.out.println(++data); // 11
+        System.out.println(data); // 11
 
     }
 }
