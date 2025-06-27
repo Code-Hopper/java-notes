5f9b0909fd2887d149780841cf159d60ec1a8585
code-hopper <ameykhondekar01@gmail.com>
2025-06-27 15:40:42 +0530

    conditional statement

--- lessons/5.Statment & Expressions/conditional statement/App.java
+++ lessons/5.Statment & Expressions/conditional statement/App.java
@@ -2,14 +2,44 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        // wap in java to check weather user is eligible for driving or not
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please Enter Your Age");
-        int age = scanner.nextInt();
-        if(age > 18){
-            System.out.println("You is eligible to drive !");
-        }else{
-            System.out.println("You are not eligible to drive");
+        // // wap in java to check weather user is eligible for driving or not
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.println("Please Enter Your Age");
+        // int age = scanner.nextInt();
+        // if(age > 18){
+        // System.out.println("You is eligible to drive !");
+        // }else{
+        // System.out.println("You are not eligible to drive");
+        // }
+
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int d = 40;
+
+        if (a > b) {
+            if (a > c) {
+                if (a > d) {
+                    System.out.println("A is greatest !");
+                } else {
+                    System.out.println("D is greatest ! line 1");
+                }
+            } else if (c > d) {
+                System.out.println("C is greatest !");
+            } else {
+                System.out.println("D is greatest ! line 2");
+            }
+        } else if (b > c) {
+            if (b > d) {
+                System.out.println("B is greatest !");
+            } else {
+                System.out.println("D is greatest ! line 3");
+            }
+        } else if (c > d) {
+            System.out.println("C is greatest !");
+        } else {
+            System.out.println("D is greatest ! line 4");
         }
-    }    
+
+    }
 }
