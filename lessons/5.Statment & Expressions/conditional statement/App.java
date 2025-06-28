1555bc2d6ab48684071cbc29e856bbed46386659
code-hopper <ameykhondekar01@gmail.com>
2025-06-28 11:56:40 +0530

    switch case and logical oprator use case

--- lessons/5.Statment & Expressions/conditional statement/App.java
+++ lessons/5.Statment & Expressions/conditional statement/App.java
@@ -12,34 +12,115 @@ public class App {
         // System.out.println("You are not eligible to drive");
         // }
 
-        int a = 10;
-        int b = 20;
-        int c = 30;
-        int d = 40;
+        // int a = 100;
+        // int b = 100;
+        // int c = 100;
+        // int d = 100;
 
-        if (a > b) {
-            if (a > c) {
-                if (a > d) {
-                    System.out.println("A is greatest !");
-                } else {
-                    System.out.println("D is greatest ! line 1");
-                }
-            } else if (c > d) {
-                System.out.println("C is greatest !");
-            } else {
-                System.out.println("D is greatest ! line 2");
-            }
-        } else if (b > c) {
-            if (b > d) {
-                System.out.println("B is greatest !");
-            } else {
-                System.out.println("D is greatest ! line 3");
-            }
-        } else if (c > d) {
-            System.out.println("C is greatest !");
-        } else {
-            System.out.println("D is greatest ! line 4");
-        }
+        // if ((a > b) && (a > c) && (a > d)) {
+        // System.out.println("A is greater than B,C & D");
+        // } else if ((b > a) && (b > c) && (b > d)) {
+        // System.out.println("B is greater than A,C & D");
+        // } else if ((c > a) && (c > b) && (c > d)) {
+        // System.out.println("C is greater than A,B & D");
+        // } else if ((d > a) && (d > b) && (d > c)) {
+        // System.out.println("D is greater than A,B & C");
+        // } else {
+        // System.out.println("All are equal !");
+        // }
+
+        // if (a > b) {
+        // if (a > c) {
+        // if (a > d) {
+        // System.out.println("A is greatest !");
+        // } else {
+        // System.out.println("D is greatest ! line 1");
+        // }
+        // } else if (c > d) {
+        // System.out.println("C is greatest !");
+        // } else {
+        // System.out.println("D is greatest ! line 2");
+        // }
+        // } else if (b > c) {
+        // if (b > d) {
+        // System.out.println("B is greatest !");
+        // } else {
+        // System.out.println("D is greatest ! line 3");
+        // }
+        // } else if (c > d) {
+        // System.out.println("C is greatest !");
+        // } else {
+        // System.out.println("D is greatest ! line 4");
+        // }
+
+        // String name = new Scanner(System.in).nextLine();
+
+        // if (name.isEmpty()) {
+        // System.out.println("user din't send name !");
+        // } else {
+        // System.out.println("user name is , " + name);
+        // }
+
+        // System.out.println("Enter Choice 1 - 4 : ");
+
+        // int choice = new Scanner(System.in).nextInt();
+        // switch (choice) {
+        // case 1:
+        // System.out.println("case 1 is selected !");
+        // break;
+        // case 2:
+        // System.out.println("case 2 is selected !");
+        // break;
+        // case 3:
+        // System.out.println("case 3 is selected !");
+        // break;
+        // case 4:
+        // System.out.println("case 4 is selected !");
+        // break;
+        // default:
+        // System.out.println("none of the above is selected !");
+        // }
+
+        // Scanner scan = new Scanner(System.in);
+        // System.out.println("Enter Number 1 : ");
+        // int number1 = scan.nextInt();
+        // System.out.println("Enter Number 2 : ");
+        // int number2 = scan.nextInt();
+
+        // System.out.println("enter choice 1: Addition, 2: Subtraction , 3: Multiplication, 4: Division, 5: Modulus : ");
+
+        // int choice = scan.nextInt();
+
+        // switch (choice) {
+        //     case 1:
+        //         System.out.println("Addition of numbers is : " + (number1 + number2));
+        //         break;
+        //     case 2:
+        //         System.out.println("Subtraction of numbers is : " + (number1 - number2));
+        //         break;
+        //     case 3:
+        //         System.out.println("Multiplication of numbers is : " + (number1 * number2));
+        //         break;
+        //     case 4:
+        //         System.out.println("Division of numbers is : " + (number1 / number2));
+        //         break;
+        //     case 5:
+        //         System.out.println("Modulus of numbers is : " + (number1 % number2));
+        //         break;
+
+        //     default:
+        //         System.out.println("wrong choice entered Please Enter Choice from (1 - 5) !");
+        //         break;
+        // }
+
+        // int age = 20;
+        // boolean learning = false;
+
+        // if(age > 18 || learning == true){
+        //     System.out.println("can drive");
+        // }else{
+        //     System.out.println("Can't drive");
+        // }
 
     }
 }
