84ab20e9a8ba9778ec4bfc06269f35a0fe3ff744
“code-hopper” <ameykhondekar01@gmail.com>
2025-09-12 07:46:08 +0530

    custom exception

--- lessons/10.exception_handling/Custom.java
+++ lessons/10.exception_handling/Custom.java
@@ -1,3 +1,23 @@
 public class Custom {
-       
+
+    public static void doSomething(int data) throws Exception {
+        if (data < 10) {
+            System.out.println("all ok !");
+        } else {
+            throw new Exception("Data is not less than 10 !");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("doing something !");
+        try {
+            doSomething(18);
+        } catch (Exception e) {
+            System.out.println("an exception occured !");
+            System.out.println(e);
+        } finally {
+            System.out.println("programm completed !");
+        }
+    }
+
 }
