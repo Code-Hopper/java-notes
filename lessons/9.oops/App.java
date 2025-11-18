5f265df5d470a127fd76037bc94df24ce76eb9aa
“code-hopper” <ameykhondekar01@gmail.com>
2025-11-18 20:04:06 +0530

    oop

--- lessons/9.oops/App.java
+++ lessons/9.oops/App.java
@@ -1,21 +1,78 @@
-class Animal {
-    String name = "some name";
-    int legs = 3;
-    boolean canWalk = true;
+import java.util.*;
+
+class birds {
+    // properties
+    int wings = 2;
+    int legs = 2;
+    String name = "just birds";
     boolean canFly = true;
 
-    void displayData() {
-        System.out.println("animal name is " + name);
-        System.out.println("this animal have " + legs + " legs");
-        System.out.println("can this animal walk ? " + canWalk);
-        System.out.println("can this animal fly ? " + canFly);
+    // constructors
+    // we can use a constructor as primary getter method
+    birds(int wings, int legs, String name, boolean canFly) {
+        // this keyword
+        this.wings = wings;
+        this.legs = legs;
+        this.name = name;
+        this.canFly = canFly;
+    }
+
+    // getter/setter
+
+    // setter
+    // void createBirdInfo(int wings, int legs, String name, boolean canFly) {
+    // // this keyword
+    // this.wings = wings;
+    // this.legs = legs;
+    // this.name = name;
+    // this.canFly = canFly;
+    // }
+
+    // methods
+    // getter
+    void info() {
+        System.out.println("hello i'm " + name + " bird !");
+        System.out.println("i have " + wings + " wings.");
+        System.out.println("i have " + legs + " legs.");
+        if (canFly) {
+            System.out.println("i can fly !");
+        } else {
+            System.out.println("i can't fly !");
+        }
+    }
+
+    void eat() {
+        System.out.println(name + " bird is eating !");
+    }
+
+    void sleep() {
+        System.out.println(name + " bird is sleeping !");
+    }
+
+    void fly() {
+        System.out.println(name + " bird is flying !");
     }
 }
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("hello");
-        Animal animal1 = new Animal();
-        System.out.println(animal1.name);
+        birds bird1 = new birds(2, 2, "sparrow", true);
+
+        birds bird2 = new birds(4, 2, "hen", false);
+
+        birds bird3 = new birds(2, 2, "Eagle", true);
+
+        // bird1.createBirdInfo(2, 2, "sparrow", true);
+        // bird2.createBirdInfo(4, 2, "hen", false);
+        // bird3.createBirdInfo(2, 2, "Eagle", true);
+
+        bird1.info();
+        bird1.eat();
+        bird1.sleep();
+
+        bird2.info();
+
+        bird3.info();
+
     }
 }
